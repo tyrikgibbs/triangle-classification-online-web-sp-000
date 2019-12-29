65fceffb17c2508b9124526ca230f358c9f31e29
Tyrik Gibbs <tyrikgibbs@outlook.com>
2019-12-29 01:46:35 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,4 +1,8 @@
 class Triangle
+
+  class TriangleError < StandardError
+
+  end
   
   def initialize(side_1, side_2, side_3)
     @triangle_sides = []
@@ -33,8 +37,4 @@ class Triangle
       raise TriangleError
     end
   end
-end
-
-class TriangleError < StandardError
-
 end
