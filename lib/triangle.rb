963e047ccb22178eaa97791708c882c2a1c0354a
Tyrik Gibbs <tyrikgibbs@outlook.com>
2019-12-29 01:15:00 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,33 +1,36 @@
 class Triangle
-  attr_accessor :x, :y, :z
-  def initialize(x, y, z)
-    @x = x
-    @y = y
-    @z = z
+  def initialize(side_1, side_2, side_3)
+    @triangle_sides = []
+    @triangle_sides << side_1
+    @triangle_sides << side_2
+    @triangle_sides << side_3
+  end
+
+  def valid?
+    sum_one_two = @triangle_sides[0] + @triangle_sides[1]
+    sum_one_three = @triangle_sides[0] + @triangle_sides[2]
+    sum_two_three = @triangle_sides[1] + @triangle_sides[2]
+
+    if (@triangle_sides.none? {|side| side <= 0}) &&
+      (sum_one_two > @triangle_sides[2] && sum_one_three > @triangle_sides[1] && sum_two_three > @triangle_sides[0])
+      return true
+    else
+      return false
+    end
   end
 
   def kind
-    if kosher_triangle == true && kosher_triangle2 == true
-      puts true
+    if valid?
+      if @triangle_sides.uniq.length == 1
+        return :equilateral
+      elsif @triangle_sides.uniq.length == 2
+        return :isosceles
+      else
+        return :scalene
+      end
     else
       raise TriangleError
     end
-
-    if x == y && y == z && x == z
-      :equilateral
-    elsif x != y && y != z && z != x
-      :scalene
-    else
-      :isosceles
-    end
-  end
-
-  def kosher_triangle
-    x.positive? && y.positive? && z.positive?
-  end
-
-  def kosher_triangle2
-    x + y > z && x + z > y && y + z > x
   end
 end
 
